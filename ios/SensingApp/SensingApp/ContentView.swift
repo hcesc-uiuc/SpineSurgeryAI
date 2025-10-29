f6446d4fd8dd952c686d966f5c09836ee596cc43
Mashfiqui <mrabbi@illinois.edu>
2025-10-29 16:39:07 -0500

    Added sample ResearchKitSwiftUI example

---
+++ ios/SensingApp/SensingApp/ContentView.swift
@@ -0,0 +1,374 @@
+//
+//  ContentView.swift
+//  SensingTrialApp
+//
+//  Created by Mashfiqui Rabbi on 10/4/25.
+//
+
+import SwiftUI
+import CoreMotion
+internal import Combine
+import BackgroundTasks
+import ResearchKitSwiftUI
+
+///
+///
+///
+struct ContentView: View {
+    @StateObject private var motionManager = MotionManager()
+    @State var showDeniedAlert = false
+    @Environment(\.scenePhase) var scenePhase
+    let motionActivityManager = CMMotionActivityManager()
+    //let recorder = CMSensorRecorder()
+    
+    
+//    var survey: ResearchForm = ResearchForm(
+//        id: "SurveyTask",
+//        steps: {
+//            ResearchFormStep(
+//                title: "Demographics",
+//                subtitle: "Tell us about yourself",
+//                content: {
+//                    TextQuestion(
+//                        id: "textQuestion",
+//                        title: "What is your name?",
+//                        prompt: "Enter your name here",
+//                        lineLimit: .singleLine,
+//                        characterLimit: 0
+//                    )
+//                    .questionRequired(true)
+//                }
+//            )
+//        },
+//        onResearchFormCompletion: { completion in
+//            switch completion {
+//            case .completed(let results):
+//                print(results)
+//            case .discarded:
+//                print("cancelled")
+//            default:
+//                print("cancelled")
+//            }
+//        }
+//    )
+
+    @State private var isPresented = false
+    @Environment(\.dismiss) var dismiss
+    var body: some View {
+        
+        
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            
+            Text("\nAccelerometer")
+            if let data = motionManager.accelerometerData {
+                Text("x: \(data.acceleration.x, specifier: "%.4f")")
+                Text("y: \(data.acceleration.y, specifier: "%.4f")")
+                Text("z: \(data.acceleration.z, specifier: "%.4f")")
+            } else {
+                Text("No motion found").foregroundStyle(Color.red)
+            }
+            
+            Text("\nGyroscope")
+            if let gyroData = motionManager.gyroscopeData {
+                Text("x: \(gyroData.rotationRate.x, specifier: "%.4f")")
+                Text("y: \(gyroData.rotationRate.y, specifier: "%.4f")")
+                Text("z: \(gyroData.rotationRate.z, specifier: "%.4f")")
+            } else {
+                Text("Gyro not found").foregroundStyle(Color.red)
+            }
+            Button("Fetch data"){
+                Task{
+                    await self.fetchRecordedData()
+                }
+            }.padding(.top, 30)
+            Button("Print schedule bg task"){
+                Task{
+                    BackgroundScheduler.shared.printScheduledBackgroundTasks()
+                }
+            }.padding(.top, 30)
+            Button("Print log data"){
+                Task{
+                    self.printCurrentLogFile()
+                }
+            }.padding(.top, 30)
+            VStack {
+                Button("Start Survey") {
+                    isPresented = true
+                }
+                //https://swiftpackageindex.com/stanfordbdhg/researchkit/4.0.0-beta.2/documentation/researchkitswiftui
+                //https://github.com/ResearchKit/ResearchKit/issues/1573
+                .sheet(isPresented: $isPresented) {
+                    ResearchForm(
+                        id: "SurveyTask",
+                        steps: {
+                            ResearchFormStep(
+                                title: "Demographics",
+                                subtitle: "Tell us about yourself",
+                                content: {
+                                    TextQuestion(
+                                        id: "Q1",
+                                        title: "What is your name?",
+                                        prompt: "Enter your name here",
+                                        lineLimit: .singleLine,
+                                        characterLimit: 0
+                                    )
+                                    .questionRequired(true)
+                                }
+                            )
+                            ResearchFormStep(
+                                title: "Yes or no question",
+                                subtitle: "Tell us about yourself",
+                                content: {
+                                    MultipleChoiceQuestion(
+                                        id: "Q2",
+                                        title: "What is your name?",
+                                        choices: [
+                                            TextChoice(
+                                                id: "Q21",
+                                                choiceText: "Yes",
+                                                value: 1
+                                            ),
+                                            TextChoice(
+                                                id: "Q22",
+                                                choiceText: "No",
+                                                value: 0
+                                            )
+                                        ], choiceSelectionLimit: .single
+                                    )
+                                    .questionRequired(true)
+                                }
+                            )
+                        },
+                        onResearchFormCompletion: { completion in
+                            switch completion {
+                                case .completed(let results):
+                                    print(results)
+                                    //save(results)
+                                    self.isPresented = false
+                                case .discarded:
+                                    print("cancelled")
+                                default:
+                                    print("cancelled")
+                            }
+                        }
+                    )
+                }
+            }
+            
+        }
+        .padding(50)
+        .alert(isPresented: $showDeniedAlert) {
+            //We are doing this, if the user denied access then we will
+            //ask users to enable it.	
+            Alert(
+                title: Text("Motion Access Denied"),
+                message: Text("Please go to Settings > Privacy & Security > " +
+                              "Motion & Fitness to enable access for this app.")
+            )
+        }.onLoad {
+            print("Calling onLoad")
+            //After the application loads, we ask/check for permission.
+            checkMotionAndFitnessAuthorization()
+            //registerBackgroundTasks()
+        }.onChange(of: scenePhase) { oldPhase, newPhase in
+            if newPhase == .background {
+                print("App moved to background")
+                BackgroundScheduler.shared.scheduleAppRefresh()
+                startBackgroundRecordingTask()
+                Logger.shared.append("App moved to background")
+                // Perform actions when the app enters the background
+            } else if newPhase == .active {
+                print("App moved to foreground")
+                Logger.shared.append("App moved to foreground")
+                // Perform actions when the app enters the foreground
+            } else if newPhase == .inactive {
+                print("App is inactive")
+                Logger.shared.append("App moved to inactive")
+                // Perform actions when the app becomes inactive (e.g., during a phone call)
+            }
+        }
+        
+        //
+        // I can pretty much consider that I am here, view already loaded
+        // There is not onViewLoad in swiftUI anymore
+        // https://stackoverflow.com/questions/56496359/swiftui-view-viewdidload
+        //
+        //
+        //        Button("Permit accelerometer\nbackground recording") {
+        //            // This closure contains the code that executes when the button is tapped.
+        //            requestMotionPermission()
+        //            // You can perform any action here, such as updating state, navigating, etc.
+        //        }
+        //
+
+    }
+    
+    func checkMotionAndFitnessAuthorization() {
+        let status = CMMotionActivityManager.authorizationStatus()
+        
+        switch status {
+            case .notDetermined:
+                print("Motion & Fitness permission is not determined.")
+                requestMotionPermission()
+            case .authorized:
+                print("Motion & Fitness permission is authorized. Started recording.")
+                // Proceed with your motion-related tasks
+                // startAccelerometerUpdates()
+                //No need to start as we have already started it
+                //startBackgroundRecordingTask()
+            case .denied:
+                print("Motion & Fitness permission is denied.")
+                // Guide the user to re-enable in Settings
+                showPermissionDeniedAlert()
+            case .restricted:
+                print("Motion & Fitness permission is restricted.")
+            @unknown default:
+                print("Unknown authorization status")
+        }
+    }
+    
+    
+//================================================================================================================
+//
+//  Internal
+//
+//================================================================================================================
+    
+    
+    
+    // Ask for motion authorization (needed for CMSensorRecorder)
+    func requestMotionPermission() {
+        guard CMMotionActivityManager.isActivityAvailable() else { return }
+        motionActivityManager.queryActivityStarting(from: Date(), to: Date(), to: .main) { _, error in
+            // This call is just to trigger the permission dialog
+            print("Motion permission requested.")
+            DispatchQueue.main.async {
+                let status = CMMotionActivityManager.authorizationStatus()
+                switch status {
+                case .authorized:
+                    print("✅ Motion permission granted")
+                    AcclerometerRecorder.shared.startRecording()
+                    //completion(true)
+                case .denied, .restricted:
+                    print("❌ Motion permission denied/restricted")
+                    //completion(false)
+                case .notDetermined:
+                    print("⏳ Motion permission not determined yet")
+                    //completion(false)
+                @unknown default:
+                    print("Default")
+                }
+            }
+        }
+    }
+    
+    func startBackgroundRecordingTask() {
+        if CMSensorRecorder.isAccelerometerRecordingAvailable() {
+            // Record accelerometer data for 12 hours (max allowed)
+            //Note we already registered at the SensingAppApp task.
+            BackgroundScheduler.shared.scheduleBGProcessingTask()
+            //print("Started accelerometer recording.")
+        } else {
+            print("Accelerometer recording not available on this device.")
+        }
+    }
+    func showPermissionDeniedAlert() {
+        showDeniedAlert = true
+    }
+    func fetchRecordedData() async {
+        AcclerometerRecorder.shared.fetchRecordedData1Min()
+    }
+    
+    func printCurrentLogFile(){
+        print("Current log file:", Logger.shared.currentLogFilePath())
+        if let logs = Logger.shared.readAll() {
+            print(logs)
+        }
+    }
+    
+    
+
+    
+    
+}
+
+#Preview {
+    ContentView()
+}
+
+
+
+
+///
+/// This observable is for showing data in the UI
+///
+class MotionManager: ObservableObject {
+    
+    private let motion = CMMotionManager() // Core motion manager instance
+    @Published var accelerometerData: CMAccelerometerData? // Published data for SwiftUI updates
+    @Published var gyroscopeData: CMGyroData? // Published gyroscope data for SwiftUI updates
+    
+    init() {
+        startAcceleromoterUpdates() // Start accelerometer updates
+        startGyroscopeUpdates() // Start gyroscope updates
+    }
+
+    // Function to start accelerometer updates
+    func startAcceleromoterUpdates() {
+        if motion.isAccelerometerAvailable {
+            motion.accelerometerUpdateInterval = 0.1 // Updates every 0.1 seconds
+            motion.startAccelerometerUpdates(to: .main) { [weak self] data, error in
+                if let data = data {
+                    self?.accelerometerData = data // Update accelerometer data
+                }
+            }
+        }
+    }
+
+    // Function to start gyroscope updates
+    func startGyroscopeUpdates() {
+        if motion.isGyroAvailable {
+            motion.gyroUpdateInterval = 0.1 // Updates every 0.1 seconds
+            motion.startGyroUpdates(to: .main) { [weak self] data, error in
+                if let data = data {
+                    self?.gyroscopeData = data // Update gyroscope data
+                }
+            }
+        }
+    }
+}
+
+struct ViewDidLoadModifier: ViewModifier {
+    @State private var didLoad = false
+    private let action: (() -> Void)?
+
+    init(perform action: (() -> Void)? = nil) {
+        self.action = action
+    }
+    func body(content: Content) -> some View {
+        content.onAppear {
+            if didLoad == false {
+                didLoad = true
+                action?()
+            }
+        }
+    }
+}
+
+extension View {
+    func onLoad(perform action: (() -> Void)? = nil) -> some View {
+        modifier(ViewDidLoadModifier(perform: action))
+    }
+}
+
+extension CMSensorDataList: @retroactive Sequence {
+    public typealias Iterator = NSFastEnumerationIterator
+    public func makeIterator() -> NSFastEnumerationIterator {
+        return NSFastEnumerationIterator(self)
+    }
+}
+
+
---
+++ ios/SensingApp/SensingApp/SensingAppApp.swift
@@ -0,0 +1,125 @@
+//
+//  SensingAppApp.swift
+//  SensingApp
+//
+//  Created by Mohammod Mashfiqui Rabbi Shuvo on 10/16/25.
+//
+
+import SwiftUI
+import CoreData
+import BackgroundTasks
+
+@main
+struct SensingAppApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
+    init(){
+        print("SensingApp init called")
+        
+        Logger.shared.append("")
+        Logger.shared.append("=======================================================")
+        Logger.shared.append("SensingApp init called")
+        
+        //        BackgroundScheduler.shared.registerBackgroundTasks()
+        //        BackgroundScheduler.shared.scheduleBGProcessingTask()
+        //
+        //        BackgroundScheduler.shared.registerBackgroundAppRefreshTask()
+        //        BackgroundScheduler.shared.scheduleAppRefresh()
+        
+    }
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    
+    ///
+    /// What does Application functions do?
+    ///
+    /// The system calls specific application(_:didSomething:) functions on your AppDelegate at key moments.
+    /// Each one handles a particular system event.
+    ///
+    
+    
+    // Called when app launches
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil
+    ) -> Bool {
+        print("App launched")
+        registerForPushNotifications()
+        // Start background location updates immediately
+        // LocationManager.shared.start()
+        BackgroundScheduler.shared.registerBackgroundTasks()
+        BackgroundScheduler.shared.registerBackgroundAppRefreshTask()
+        return true
+    }
+    
+    /// Request notification permission and register with APNs
+    func registerForPushNotifications() {
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) { granted, error in
+            print("Permission granted: \(granted)")
+            
+            guard granted else { return }
+            DispatchQueue.main.async {
+                UIApplication.shared.registerForRemoteNotifications()
+            }
+        }
+    }
+    
+    /// Called when APNs registration succeeds — this gives you the DEVICE TOKEN
+    func application(_ application: UIApplication,
+                     didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data)  {
+        let tokenParts = deviceToken.map { String(format: "%02.2hhx", $0) }
+        let token = tokenParts.joined()
+        print("✅ Device Token: \(token)")
+        Task { @MainActor in
+            let responseText = await UploadToServer.shared.uploadDeviceTokenToServer(deviceToken: token)
+            print("responseText \(responseText)")
+            Logger.shared.append("APNS registration successful")
+            Logger.shared.append("Device Token: \(token)")
+        }
+    }
+    
+
+    /// Called if APNs registration fails
+    func application(_ application: UIApplication,
+                     didFailToRegisterForRemoteNotificationsWithError error: Error) {
+        print("❌ Failed to register: \(error.localizedDescription)")
+    }
+    
+    
+    func application(_ application: UIApplication,
+                         didReceiveRemoteNotification userInfo: [AnyHashable: Any],
+                     fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
+        print("Received notification: \(userInfo)")
+        
+        // Extract custom data from the silent notification
+        if let data = userInfo["data"] as? [String: Any] {
+            print("Silent notification received:", data)
+            Logger.shared.append("Silent notification received")
+        }
+        
+        BackgroundScheduler.shared.printScheduledBackgroundTasks()
+        BackgroundScheduler.shared.scheduleAppRefresh()
+    }
+    
+    
+    func applicationDidEnterBackground(_ application: UIApplication) {
+        print("App moved to background")
+    }
+
+    func applicationWillEnterForeground(_ application: UIApplication) {
+        print("App returning to foreground")
+    }
+    
+    /// Application is about to terminate
+    func applicationWillTerminate(_ application: UIApplication) {
+        print("App is about to terminate")
+    }
+}
